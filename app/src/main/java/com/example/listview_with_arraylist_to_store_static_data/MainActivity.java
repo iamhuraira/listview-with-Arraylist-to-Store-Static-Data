857d6bc9012c3e3fd1cca76c3459e48e271560c0
Abu Huraira <iamhuraira429@gmail.com>
2022-05-25 23:29:29 +0500

    Creating Project For Arrylist To Store Data and Show In The List Box

---
+++ app/src/main/java/com/example/listview_with_arraylist_to_store_static_data/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.listview_with_arraylist_to_store_static_data;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
