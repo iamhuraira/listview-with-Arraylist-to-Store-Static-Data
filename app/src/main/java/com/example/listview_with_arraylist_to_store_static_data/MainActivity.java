031e1b514a8e8463165c9aa6485416e1a987e256
Abu Huraira <iamhuraira429@gmail.com>
2022-05-27 00:38:13 +0500

    Working In the MainActivity.java for Functionality

--- app/src/main/java/com/example/listview_with_arraylist_to_store_static_data/MainActivity.java
+++ app/src/main/java/com/example/listview_with_arraylist_to_store_static_data/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.listview_with_arraylist_to_store_static_data;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn;
+    ListView lstview;
+    EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void enterNameToList(View view) {
     }
 }
